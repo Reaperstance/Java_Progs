2c608af1ca7b061e882755de00778d1432b25fb4
Subham Das <83867874+Reaperstance@users.noreply.github.com>
2025-03-07 01:20:04 +0530

    Add files via upload

---
+++ alpha.java
@@ -0,0 +1,15 @@
+class alpha 
+{
+    public static void main(String[] args) 
+    {
+        char c;
+        char d='a';
+        for(c = 'A'; c <= 'Z'; ++c)
+        {
+            System.out.print(c + "-" + d + "\n");
+            d++;
+
+        }
+        
+    }
+  }
